b0385e7b57e559691cb194ca0418095c3bfcaa9f
Labanter13 <93643484+Labanter13@users.noreply.github.com>
2025-08-12 01:31:43 +0300

    Implement key collection and multiplier ladder

--- games/kyv_lab_titans/gamestate.py
+++ games/kyv_lab_titans/gamestate.py
@@ -10,9 +10,32 @@ class GameState(GameStateOverride):
         self.reset_seed(sim)
         self.repeat = True
         while self.repeat:
+            # Reset simulation and key tracking for a new spin
             self.reset_book()
+            self.keys_collected = 0
+            self.pending_wilds = []
+
+            self.draw_board()
+            self.spawn_wilds()
+            self.evaluate_cascade()
+            self.emit_tumble_win_events()
+            self.handle_key_boosts()
+
+            while self.win_data["totalWin"] > 0 and not (self.wincap_triggered):
+                self.tumble_game_board()
+                self.spawn_wilds()
+                self.evaluate_cascade()
+                self.emit_tumble_win_events()
+                self.handle_key_boosts()
+
+            self.set_end_tumble_event()
+            self.win_manager.update_gametype_wins(self.gametype)
+
+            if self.check_fs_condition() and self.check_freespin_entry():
+                self.run_freespin_from_base()
 
             self.evaluate_finalwin()
+            self.check_repeat()
 
         self.imprint_wins()
 
@@ -20,6 +43,22 @@ class GameState(GameStateOverride):
         self.reset_fs_spin()
         while self.fs < self.tot_fs:
             self.update_freespin()
-            pass
+            self.spawn_wilds()
+            self.evaluate_cascade()
+            self.emit_tumble_win_events()
+            self.handle_key_boosts()
+
+            while self.win_data["totalWin"] > 0 and not (self.wincap_triggered):
+                self.tumble_game_board()
+                self.spawn_wilds()
+                self.evaluate_cascade()
+                self.emit_tumble_win_events()
+                self.handle_key_boosts()
+
+            self.set_end_tumble_event()
+            self.win_manager.update_gametype_wins(self.gametype)
+
+            if self.check_fs_condition():
+                self.update_fs_retrigger_amt()
 
         self.end_freespin()
